930f44d0a73053c762c4a6b472d98400e77a9175
aeharding@gmail.com <aeharding@gmail.com@c1766da9-1b19-9a88-3601-8d11d65f8203>
2013-03-18 03:59:55 +0000

    More updates to the teacher's side processing

--- webserver/setup/newDatabase.php
+++ webserver/setup/newDatabase.php
@@ -27,5 +27,45 @@ if (!$con->query($sql)) {
 	printf("Created table 'sessions' successfully.");
 }
 
+// Create votes
+$sql = "CREATE TABLE votes 
+(
+PID INT NOT NULL AUTO_INCREMENT, 
+PRIMARY KEY(PID),
+s_sid CHAR(30),
+s_qnum CHAR(50),
+s_vote CHAR(150)
+)";
+
+// Execute query
+if (!$con->query($sql)) {
+	printf("Error message: %s\n", $con->error);
+} else {
+	printf("Created table 'votes' successfully.");
+}
+
+// Create questions
+/* Different types:
+	1. mult [A-E answers]
+	2. bool [true/false]
+	3. resp [up to 150 characters]
+*/
+$sql = "CREATE TABLE questions 
+(
+PID INT NOT NULL AUTO_INCREMENT, 
+PRIMARY KEY(PID),
+s_sid CHAR(30),
+s_qtype CHAR(4),
+s_correct CHAR(1),
+s_qdesc CHAR(150)
+)";
+
+// Execute query
+if (!$con->query($sql)) {
+	printf("Error message: %s\n", $con->error);
+} else {
+	printf("Created table 'questions' successfully.");
+}
+
 $con->close();
 ?>
