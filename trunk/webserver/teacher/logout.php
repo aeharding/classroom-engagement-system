eb90bc3d35eb5eb45e5481fd19427f9b919d6824
aeharding@gmail.com <aeharding@gmail.com@c1766da9-1b19-9a88-3601-8d11d65f8203>
2013-03-18 23:08:18 +0000

    Small bug fix.

--- trunk/webserver/teacher/logout.php
+++ trunk/webserver/teacher/logout.php
@@ -1,6 +1,11 @@
 <?php
 
 	session_start(); 
+	if(isset($_SESSION['sessionActive'] && $_SESSION['sessionActive'])) {
+		$con = new mysqli("localhost","appcooki_voteadm","trace","appcooki_vote");
+		$query = "UPDATE sessions SET s_isOpen='0' WHERE s_sid='" . $_SESSION['session'] . "'";
+		$result = $con->query($query);
+	}
 	session_destroy();
 	header("location:login.php");
 	
