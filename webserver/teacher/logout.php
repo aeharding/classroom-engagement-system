157f747f7287a64c241fcbb9923f55831668b692
aeharding@gmail.com <aeharding@gmail.com@c1766da9-1b19-9a88-3601-8d11d65f8203>
2013-03-19 02:42:33 +0000

    Fixed bugs

--- webserver/teacher/logout.php
+++ webserver/teacher/logout.php
@@ -1,7 +1,7 @@
 <?php
 
 	session_start(); 
-	if(isset($_SESSION['sessionActive'] && $_SESSION['sessionActive'])) {
+	if(isset($_SESSION['sessionActive']) && $_SESSION['sessionActive']) {
 		$con = new mysqli("localhost","appcooki_voteadm","trace","appcooki_vote");
 		$query = "UPDATE sessions SET s_isOpen='0' WHERE s_sid='" . $_SESSION['session'] . "'";
 		$result = $con->query($query);
