431691aed553f066579edf1594087ac1f082e8c0
aeharding@gmail.com <aeharding@gmail.com@c1766da9-1b19-9a88-3601-8d11d65f8203>
2013-03-26 04:04:13 +0000

    Button loading period *never* resets.

--- webserver/student/index.php
+++ webserver/student/index.php
@@ -91,9 +91,6 @@
 				.click(function () {
 						var btn = $(this)
 						btn.button('loading')
-						setTimeout(function () {
-								btn.button('reset')
-						}, 3000)
 				});
 			});
     </script>
